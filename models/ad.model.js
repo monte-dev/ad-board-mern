782821af9da7a37acf41c30250688e97b59f544e
monte-dev <badziobw@gmail.com>
2023-08-08 23:03:41 +0100

    Add User and Advert models

---
+++ models/ad.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const advertSchema = new mongoose.Schema({
+	title: { type: String, required: true, minlength: 10, maxlength: 50 },
+	content: { type: String, required: true, minlength: 20, maxlength: 1000 },
+	publishedDate: { type: String, required: true },
+	image: { type: String, required: true },
+	price: { type: Number, required: true },
+	location: { type: String, required: true },
+	seller: { type: String, required: true, ref: 'User' },
+});
+
+module.exports = mongoose.model('Advert', advertSchema);
