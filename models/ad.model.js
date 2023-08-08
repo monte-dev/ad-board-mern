1627826a06f11260b7e61eb06fb3eb046669aa8b
monte-dev <badziobw@gmail.com>
2023-08-08 23:13:08 +0100

    Fix: data types in models

--- models/ad.model.js
+++ models/ad.model.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const advertSchema = new mongoose.Schema({
 	title: { type: String, required: true, minlength: 10, maxlength: 50 },
 	content: { type: String, required: true, minlength: 20, maxlength: 1000 },
-	publishedDate: { type: String, required: true },
+	publishedDate: { type: Date, default: Date.now, required: true },
 	image: { type: String, required: true },
 	price: { type: Number, required: true },
 	location: { type: String, required: true },
-	seller: { type: String, required: true, ref: 'User' },
+	seller: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'User',
+	},
 });
 
 module.exports = mongoose.model('Advert', advertSchema);
--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	login: { type: String, required: true },
+	login: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	avatar: { type: String, required: true },
-	phoneNumber: { type: Number, required: true },
+	phoneNumber: { type: String, required: true },
 });
 
 module.exports = mongoose.model('User', userSchema);
