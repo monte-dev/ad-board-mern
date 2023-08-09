b68ff88d04aee1f7329c1b8802a3ad135570bd84
monte-dev <badziobw@gmail.com>
2023-08-09 23:40:43 +0100

    Add ads get routes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+const AdvertController = require('../controllers/ads.controller');
+
+router.get('/ads', AdvertController.getAll);
+router.get('/ads/:id', AdvertController.getById);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const formidable = require('express-formidable');
 const db = require('./db');
-
+const adsRoutes = require('./routes/ads.routes');
 const app = express();
 const server = app.listen(process.env.PORT || 8000, () => {
 	console.log('Server is running...');
@@ -18,6 +18,8 @@ db();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', adsRoutes);
 // serve files from client side - React App
 
 app.use(express.static(path.join(__dirname, '/client/build')));
